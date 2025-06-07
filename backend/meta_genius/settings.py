15bc2ac38639f5843386d82ada402563d11837ee
Brian Njuguna <142185778+001kenji@users.noreply.github.com>
2025-06-07 20:27:53 +0300

    Update settings.py

--- backend/meta_genius/settings.py
+++ backend/meta_genius/settings.py
@@ -360,8 +360,8 @@ CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',') if not D
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'machariabrian712@gmail.com'
-EMAIL_HOST_PASSWORD= 'tmni cnhb nqho csqs'
+EMAIL_HOST_USER = '......'
+EMAIL_HOST_PASSWORD= '......'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL =  'noreply@metagenius.ai'
 
